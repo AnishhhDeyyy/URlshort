efa7d30f787dfd330701f8fda8bf5317472e20ae
Anish Chandra Dey <anishdey400@gmail.com>
2025-01-17 14:50:43 +0530

    DONE

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const { sequelize } = require("./dbconnect");
 const { URRL } = require("./models/urldata");
@@ -6,6 +7,14 @@ const { router } = require("./routes/url");
 
 const app = express();
 app.use(express.json());
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"))
+
+app.get('/test',async(req,res)=>{
+    const allurls = await URRL.findAll();
+    console.log(allurls);
+    res.render("home",{urls:allurls})
+})
 //sequelize.drop()
 app.use("/url",router);
 URRL.sync({force:false})
